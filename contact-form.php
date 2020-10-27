5bc9a9acbcb0652a755ee3d7139b2b27c989e922
Singh Karan Narendra <73518312+karannsingh@users.noreply.github.com>
2020-10-27 17:30:19 +0530

    silodoctor

---
+++ contact-form.php
@@ -0,0 +1,26 @@
+﻿<?php
+
+	$UserName = $_POST['Name'];
+	$Email = $_POST['Email'];
+	$Phone = $_POST['Phone'];
+	$Msg = $_POST['Message'];
+
+	$email_from = 'karan.n.singh0@gmail.com';
+
+	$email_subject = 'New Form Submission';
+
+	$email_body = "User Name: $UserName.\n".
+					"User Email: $Email.\n".
+					"User Message: $Msg.\n";
+
+	$to = "karan.n.singh0@gmail.com";
+
+	$headers = "From: $email_from \r\n";
+
+	$headers = "Replay-To: $Email \r\n";
+
+	mail($to,$email_subject,$email_body,$headers);
+
+	header("Location: contact-us.html");
+	
+?>
